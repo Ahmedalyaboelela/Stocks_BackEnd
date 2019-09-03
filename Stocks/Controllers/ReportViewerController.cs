8a6f40468982a5b47d5eeea1df75f8ec0af11b04
Ahmedalyaboelela <ahmedaly1987@gmail.com>
2019-09-03 10:29:47 +0200

    upd3-9

---
+++ Stocks/Controllers/ReportViewerController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BAL.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Stimulsoft.Report;
+using Stimulsoft.Report.Dictionary;
+using Stimulsoft.Report.Mvc;
+
+namespace Stocks.Controllers
+{
+    [Authorize(Roles = "SuperAdmin,Admin")]
+    [Produces("application/json")]
+    [Route("api/ReportViewer")]
+    public class ReportViewerController : Controller
+    {
+        #region CTOR & Definitions
+        private readonly ApplicationSettings _appSettings;
+        public ReportViewerController( IOptions<ApplicationSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+        #endregion
+        #region  Retreive Reports
+
+
+
+
+        // Retrieve Resultofportofolio Report after sending parameters
+        [HttpPost]
+        [Route("~/api/ReportViewer/ResultOfPortofolio")]
+        public string ResultOfPortofolioWork()
+        {
+            StiReport report = new StiReport();
+            var path = StiNetCoreHelper.MapPath(this, "Reports/Report20.mrt");
+            report.Load(path);
+            report["@name"] = 1;
+            var dbMS_SQL = (StiSqlDatabase)report.Dictionary.Databases["MS SQL"];
+            dbMS_SQL.ConnectionString = "@" +_appSettings.Report_Connection;
+            report.Render(false);
+            return report.SaveDocumentJsonToString();
+
+        }
+        #endregion 
+    }
+}
